ef9d4da2d06a46a1044e64519f49f18d6ea2d90d
kenji <enjoylight.ap@gmail.com>
2023-09-08 14:24:15 +0530

    sol 1 ex

---
+++ examples/sol/1.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+
+contract Examples {
+
+    uint balance;
+
+    function setBalance(uint value) public{
+        balance = value;
+    }
+
+    function getBalance() public view returns(uint){
+        return balance;
+    }
+
+    function calculate(uint x,uint y) public view returns(uint){
+        return x*y;
+    }
+
+
+    function deposit(uint value) public{
+        balance = balance + value;
+    }
+
+    function withdraw(uint value) public{
+        if(balance >= value){
+            balance = balance - value;
+        }else{
+
+        }
+    }
+}
