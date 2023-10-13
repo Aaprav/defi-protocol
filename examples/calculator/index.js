f49c5fc6017aec26b053d25bb14c56e0c60ae952
kenji <enjoylight.ap@gmail.com>
2023-10-13 14:28:17 +0530

    calculator fixed

--- examples/calculator/index.js
+++ examples/calculator/index.js
@@ -5,35 +5,46 @@ let inputs = [];
 let currentInput = "";
 
   let calculate = (_input) =>{
+
     let _total = _input.reduce((i,j)=>{
+      if(!j) return i;
+
       j = j.toString();
 
+      if (j.length > 1 && j.charAt(0) == "√") {
+        j = Math.sqrt(j.substring(1));
+      }
       if(symbols.includes(j) || i.length <= 1){
-      return `${i}${j}`;
+        return `${i}${j}`;
       }else {
-      let symbol = i.charAt(i.length-1);
-      let val = i.slice(0, -1);
-      if (j.charAt(0) == "√") {
-        j = Math.sqrt(j);
-      }
 
-      if(symbol == "/"){
-        if(!j) return Infinity;
-        return val/j;
-      }else if (symbol == "x") {
-        return val*j;
-      }else if (symbol == "-") {
-        return val-j;
-      }else if (symbol == "+") {
-        return val+j;
-      }else if (symbol == "%") {
-        return val%j;
-      }
+        let symbol = i.charAt(i.length-1);
+        let val = parseFloat(i.slice(0, -1)) || 0;
+        j = parseFloat(j);
+
+        if(symbol == "/"){
+          if(!j) return Infinity;
+          return val/j;
+        }else if (symbol == "x") {
+          return val*j;
+        }else if (symbol == "-") {
+          return val-j;
+        }else if (symbol == "+") {
+          return val+j;
+        }else if (symbol == "%") {
+          return val*j/100;
+        }
       }
     },"");
-    _total = (_total || "").toString();
-    _total = _total.split("").filter(i=>!symbols.includes(i)).join("");
-    _total = Math.floor((_total || 0) * 1e6)/1e6;
+
+    if(!_total) return 0;
+    _total = _total.toString();
+
+    if(isNaN(_total.charAt(_total.length - 1))){
+      _total = _total.slice(0,-1);
+    }
+
+    _total = Math.floor((parseFloat(_total) || 0) * 1e6)/1e6;
     return _total;
   }
 
@@ -42,7 +53,7 @@ let currentInput = "";
 
 
 const renderInput = () => {
-  let _text = inputs.join("") + currentInput;
+  let _text = inputs.join("") +""+ currentInput;
   let _inputs = inputs.concat([currentInput]);
   $(".display-input").val(_text || 0);
   $(".display-sum").val(calculate(_inputs));
@@ -51,28 +62,25 @@ const renderInput = () => {
 
 
 
-
 $(".input-btn").click(function(e){
   let input = e.target.value;
   let last = inputs.length?inputs[inputs.length - 1]:"";
-  //
+
   if(input == "AC"){
     inputs = [];currentInput = "";
   }else if(input == "C"){
     if(currentInput){
-      currentInput = currentInput.length == 1?inputs.pop():currentInput.slice(0, -1);
+      currentInput = currentInput.length == 1 && !!inputs.length?inputs.pop():currentInput.slice(0, -1);
     }else if(inputs.length){
-      currentInput = inputs.pop();
+      currentInput = inputs.pop().toString();
       currentInput = currentInput.slice(0, -1);
     }
   }else if(input == "=") {
-    if(currentInput){
-      if(!isNaN(currentInput)){
-        inputs.push(currentInput)
-      }
-      currentInput = 0;
+    if(currentInput && !isNaN(currentInput)){
+      inputs.push(currentInput)
     }
-    inputs = [calculate(inputs)]
+    currentInput = calculate(inputs).toString();
+    inputs = [];
   }else if(currentInput && input != "√") {
     if(currentInput.charAt(0) == "√"){
       if(!isNaN(input)){
