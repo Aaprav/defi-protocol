302d1538b666bf1b80945cb8fb3d19f5ddb207f6
kenji <enjoylight.ap@gmail.com>
2023-10-25 02:01:14 +0530

    select

--- template/index.js
+++ template/index.js
@@ -1,1 +1,22 @@
 console.clear()
+
+$(".select-head").click(function(){
+  $(this).parent().toggleClass('show')
+})
+
+
+
+$(".select-options.normal .select-option").click(function(){
+  $(this).addClass("active").siblings().removeClass('active');
+})
+$(".select-options.collapse .select-option").click(function(){
+  $(this).addClass("active").siblings().removeClass('active').parents(".select").toggleClass('show');
+})
+$(".select-options.update .select-option").click(function(){
+  let _option = $(this).text();
+  $(this).addClass("active").siblings().removeClass('active').parent().siblings(".select-head").children(".select-selected").text(_option);
+})
+$(".select-options.update-collapse .select-option").click(function(){
+  let _option = $(this).text();
+  $(this).addClass("active").siblings().removeClass('active').parents(".select").toggleClass('show').find(".select-head .select-selected").text(_option);
+})
