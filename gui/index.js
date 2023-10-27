9fada96652d35b96c6e278ac61365ff675c32921
kenji <enjoylight.ap@gmail.com>
2023-10-27 16:06:28 +0530

    menu bar

---
+++ gui/index.js
@@ -0,0 +1,17 @@
+
+
+
+
+// document.documentElement.style.setProperty('--base-dark', "#ddd");
+// document.documentElement.style.setProperty('--base-light', "#aaa");
+//
+// document.documentElement.style.setProperty('--border-color', "#2DAB9F");
+// document.documentElement.style.setProperty('--shadow-color', "#236864");
+//
+// document.documentElement.style.setProperty('--bg-btn', "#092522");
+// document.documentElement.style.setProperty('--bg-btn-hover', "#124A44");
+//
+// document.documentElement.style.setProperty('--text-col-2', "#222");
+// document.documentElement.style.setProperty('--text-col-3', "#444");
+// document.documentElement.style.setProperty('--text-col-4', "#444");
+// document.documentElement.style.setProperty('--text-col-5', "#ccc");
