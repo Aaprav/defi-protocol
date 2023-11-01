33e344f5615fdc8cd0526eeb14ecdfbd88248315
kenji <enjoylight.ap@gmail.com>
2023-11-01 12:54:30 +0530

    wallet js fixing

--- gui/js/wallet.js
+++ gui/js/wallet.js
@@ -4,12 +4,10 @@ console.clear()
   const Wallet = {
     address:"",
     balance:0,
-    wallet:"metamask",
-
 
     connectWalletGui : () =>{
       if(Wallet.address){
-        DOM_CONNECT_WALLET.innerHTML  = helper.shortAddress(account);
+        DOM_CONNECT_WALLET.innerHTML  = helper.shortAddress(Wallet.address);
         DOM_SWAP_BTN.children[0].innerHTML  = "Swap Now";
         DOM_CONNECT_WALLET.disabled = true;
         DOM_SWAP_BTN.disabled = false;
@@ -28,15 +26,15 @@ console.clear()
       // "accounts" will always be an array, but it can be empty.
       let _address = accounts.length?accounts[0]:"";
 
-      if(!!_address && !account){
+      if(!!_address && !Wallet.address){
         setToastAlert('Wallet Connected',"success");
-      }else if (!_address && !!account) {
+      }else if (!_address && !!Wallet.address) {
         setToastAlert('Wallet Disconnected',"warning");
-      }else if(!!_address && _address != account) {
+      }else if(!!_address && _address != Wallet.address) {
         setToastAlert('Wallet Changed',"info");
       }
-      account = _address;
-      connectWalletGui();
+      Wallet.address = _address;
+      Wallet.connectWalletGui();
     },
 
     handleChainChanged : (chainId) => {
@@ -44,29 +42,28 @@ console.clear()
       // Correctly handling chain changes can be complicated.
       // We recommend reloading the page unless you have good reason not to.
       if(chainId !== '0x1'){
-        account = "";
+        Wallet.address = "";
         setToastAlert('Chain Disconnected',"warning");
+        Wallet.connectWalletGui();
       }
     },
 
 
-
+   watcherInjected:false,
+   isBtnClicked:false,
    handleConnectWallet : async(val) =>{
     try {
+      if(Wallet.address) return;
+      if(Wallet.isBtnClicked) return setToastAlert('Please wait while we proccess',"info");
+      Wallet.isBtnClicked = true;
+
       if (typeof window.ethereum === 'undefined') {
         return setToastAlert('Wallet is not installed!',"error");
       }
 
-      if(val === "metamask" && !window.ethereum.isMetaMask){
-        return setToastAlert('MetaMask is not installed!',"error");
-      }else if (val === "mathwallet" && !window.ethereum.isMathWallet) {
-        return setToastAlert('MathWallet is not installed!',"error");
-      }
-
       await new Promise(async(resolve,reject) =>{
-
         if(window.ethereum.chainId === "0x1"){
-        resolve()
+          resolve()
         }else {
           try {
             await window.ethereum.request({
@@ -80,47 +77,49 @@ console.clear()
         }
       })
 
-
       const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
       // console.log(accounts);
-      handleAccountsChanged(accounts);
+      Wallet.handleAccountsChanged(accounts);
 
-      window.ethereum.on('accountsChanged', handleAccountsChanged);
-      window.ethereum.on('chainChanged', handleChainChanged);
-
+      if(!Wallet.watcherInjected){
+        window.ethereum.on('accountsChanged', Wallet.handleAccountsChanged);
+        window.ethereum.on('chainChanged', Wallet.handleChainChanged);
+      }
+      Wallet.isBtnClicked = false;
     } catch (e) {
-      // console.log(e);
+      Wallet.isBtnClicked = false;
       let msg = e?.message??'Please authorize to access your account'
       setToastAlert(msg,"error")
     }
-
   },
 
-  isInjected:false,
+  initInjected:false,
   init : () => {
     let _iteration = 0;
-      if(!Wallet.isInjected){
-        Wallet.isInjected = true;
+      if(!Wallet.initInjected){
+        Wallet.initInjected = true;
         let timer = setInterval(async function(){
           _iteration += 1;
           if(typeof window.ethereum !== 'undefined'){
             clearInterval(timer);
             if(window.ethereum.chainId !== "0x1") {
-              Wallet.isInjected = false;
+              Wallet.initInjected = false;
             }else {
               try {
                 const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
                 Wallet.address = accounts.length?accounts[0]:"";
                 Wallet.connectWalletGui();
-                window.ethereum.on('accountsChanged', handleAccountsChanged);
-                window.ethereum.on('chainChanged', handleChainChanged);
+                if(!Wallet.watcherInjected){
+                  window.ethereum.on('accountsChanged', Wallet.handleAccountsChanged);
+                  window.ethereum.on('chainChanged', Wallet.handleChainChanged);
+                }
               } catch (e) {
-                Wallet.isInjected = false;
+                Wallet.initInjected = false;
               }
             }
           }else if (_iteration >= 5) {
             clearInterval(timer);
-            Wallet.isInjected = false;
+            Wallet.initInjected = false;
           }
         },500);
       }
@@ -130,23 +129,7 @@ console.clear()
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// DOM_CONNECT_WALLET.addEventListener('click', ()=> handleConnectWallet("metamask"));
+DOM_CONNECT_WALLET.addEventListener('click', Wallet.handleConnectWallet);
 
 document.addEventListener("DOMContentLoaded", function() {
   // Code to be executed when the DOM is ready
